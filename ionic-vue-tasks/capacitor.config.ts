7e8e796bd26ef72ab7a0c5047d8370d75744a036
Gustavo <gustavoeliashaddad@gmail.com>
2025-04-21 10:56:57 -0300

    primeiro commit

---
+++ ionic-vue-tasks/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-vue-tasks',
+  webDir: 'dist'
+};
+
+export default config;
